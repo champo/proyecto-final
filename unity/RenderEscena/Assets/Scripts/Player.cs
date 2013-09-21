e74c7d3780a07c9936e5343ec1d1fd85d78fb1ee
Esteban Ordano <eordano@gmail.com>
2013-09-21 17:10:55 -0300

    Add Unity Scene

---
+++ unity/RenderEscena/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float a;
+	public float b;
+	public float c;
+	public float d;
+	public float e;
+
+	private float t0;
+	private float tnow;
+	private float initX;
+	private float initY;
+	
+	void Start() {
+		t0 = Time.fixedTime;
+		initX = transform.position.x;
+		initY = transform.position.z;
+	}
+
+	void FixedUpdate () {
+		tnow = (Time.fixedTime - t0);
+		transform.position = new Vector3(
+			initX + c * Mathf.Sin(a * tnow),
+			transform.position.y,
+			initY + d * Mathf.Sin(b * tnow)
+		);
+	}
+}
