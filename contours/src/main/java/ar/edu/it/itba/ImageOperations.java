e9a35e7c2cfdd928dc47e123da1fd97ecce19d92
Juan Pablo Civile <elementohb@gmail.com>
2014-07-02 19:56:55 -0300

    Change color order so red isn't first

--- contours/src/main/java/ar/edu/it/itba/ImageOperations.java
+++ contours/src/main/java/ar/edu/it/itba/ImageOperations.java
@@ -26,12 +26,12 @@ public class ImageOperations {
     };
     */
 	public static Color phiColoring[] = new Color[] {
-        new Color(255, 0, 0),
-        new Color(255, 0, 255),
+        new Color(0, 0, 255),
         new Color(0, 255, 0),
         new Color(0, 255, 255),
-        new Color(255, 255, 255),
         new Color(128, 128, 128),
+        new Color(255, 0, 255),
+        new Color(255, 0, 0),
         new Color(0,0,0)
     };
     public static final Map<String, Color> teamColor = new HashMap<String, Color>();
